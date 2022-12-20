fce5bec0c368f3ffec228a31fb81382822798fc1
mdashraful <mdashrafulislam369@gmail.com>
2022-12-20 14:50:14 +0600

    add ui auth

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layout.master')
+@push('css')
+
+@endpush
+@section('header')
+    Dashboard
+@endsection
+
+@section('tagline')
+    Employee App Dashboard
+@endsection
+
+@section('content')
+
+@endsection
+
+@push('js')
+    
+@endpush
